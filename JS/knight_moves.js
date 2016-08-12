86653570ecd35ceddca1f448fdfcd0b86b01c0c9
Napear <napear@gmail.com>
2016-08-12 09:18:52 -0700

    Update knight_moves.js

--- JS/knight_moves.js
+++ JS/knight_moves.js
@@ -2,14 +2,12 @@
  * https://www.codeeval.com/browse/180/
  * CHALLENGE DESCRIPTION:
 
- In chess, the knight moves to any of the closest squares that are not on the same rank, file, or diagonal. Thus the move is in the “L” form: two squares vertically and one square horizontally, or two squares horizontally and one square vertically:
-
+ In chess, the knight moves to any of the closest squares that are not on the same rank, file, or diagonal. 
+ Thus the move is in the “L” form: two squares vertically and one square horizontally, or two squares 
+ horizontally and one square vertically:
  Your task is to find all possible positions for the next move of the knight on the empty chessboard.
-
  INPUT SAMPLE:
-
  The first argument is a filename that contains positions of the knight on the chessboard in the CN form, where:
-
  C is a letter from “a” to “h” and denotes a column.
  N is a number from 1 to 8 and denotes a row.
  Each position is indicated in a new line.
