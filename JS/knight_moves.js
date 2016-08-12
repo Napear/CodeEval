0fe04e5b4c37932a64c95a8b440bb7d656b4a964
Napear <napear@gmail.com>
2016-08-11 23:10:39 -0700

    added new solutions & some problem descriptions

---
+++ JS/knight_moves.js
@@ -0,0 +1,64 @@
+/**
+ * https://www.codeeval.com/open_challenges/180
+ * CHALLENGE DESCRIPTION:
+
+ In chess, the knight moves to any of the closest squares that are not on the same rank, file, or diagonal. Thus the move is in the “L” form: two squares vertically and one square horizontally, or two squares horizontally and one square vertically:
+
+ Your task is to find all possible positions for the next move of the knight on the empty chessboard.
+
+ INPUT SAMPLE:
+
+ The first argument is a filename that contains positions of the knight on the chessboard in the CN form, where:
+
+ C is a letter from “a” to “h” and denotes a column.
+ N is a number from 1 to 8 and denotes a row.
+ Each position is indicated in a new line.
+ */
+var fs = require("fs");
+const DIRECTIONS = [[1, 1], [1, -1], [-1, 1], [-1, -1]];
+
+function move(position, x, y) {
+  return [position[0] + x, position[1] + y];
+}
+
+function validMove(position, x, y) {
+  var testMove = move(position, x, y);
+  var output;
+  var onRow = (testMove[0] >= 97 && testMove[0] <= 104);
+  var onColumn = (testMove[1] >= 1 && testMove[1] <= 8);
+  if (onRow && onColumn) {
+    output = testMove;
+  }
+  return output;
+}
+
+function parsePosition(line) {
+  var raw = line.split('');
+  var position = [];
+  position[0] = raw[0].charCodeAt(0);
+  position[1] = parseInt(raw[1]);
+  return position;
+}
+
+function printMove(moveToPrint) {
+  return String.fromCharCode(moveToPrint[0]) + moveToPrint[1];
+}
+
+function addIfValid(validMove, moves) {
+  if (validMove) {
+    moves.push(printMove(validMove));
+  }
+}
+
+fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
+  var moves = [];
+  if (line !== "") {
+    var position = parsePosition(line);
+    for (var i = 0; i < DIRECTIONS.length; i++) {
+      var dir = DIRECTIONS[i];
+      addIfValid(validMove(position, 2 * dir[0], dir[1]), moves);
+      addIfValid(validMove(position, dir[0], 2 * dir[1]), moves);
+    }
+    console.log(moves.sort().join(' '));
+  }
+});
--- JavaSol/src/Football.java
+++ JavaSol/src/Football.java
@@ -1,12 +1,31 @@
-import java.io.*;
+/**
+ CHALLENGE DESCRIPTION:
+ People around the world watch football matches and root for different football
+ teams. Some people are fans of Real Madrid, some like Barcelona, some pull for
+ Atletico Madrid, while others do not miss a single match with FC Bayern Munich.
+ The teams would like to know people in which countries cheer for them. So,
+ let’s help them!
+ INPUT SAMPLE:
+ The first argument is a path to a file. Each row includes a test case with
+ lists of countries. Lists are separated by pipelines '|'. Each list includes
+ football teams that people in these countries root for.
+ */
+
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Football {
-    public static void main (String[] args) throws IOException {
-        File file = new File(args[0]);
-        BufferedReader buffer = new BufferedReader(new FileReader(file));
-        String line;
-        while ((line = buffer.readLine()) != null) {
-            line = line.trim();
-            // Process line of input Here
-        }
+  public static void main(String[] args) throws IOException {
+    File file = new File(args[0]);
+    BufferedReader buffer = new BufferedReader(new FileReader(file));
+    String line;
+    while ((line = buffer.readLine()) != null) {
+      line = line.trim();
+      String[] countries = line.split(" \\| ");
+
     }
+  }
 }
