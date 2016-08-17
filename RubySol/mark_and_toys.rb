daa158fec77967ed3ac871056c4ef5dc7ad3ab77
Napear <napear@gmail.com>
2016-08-16 22:27:51 -0700

    Formatted comment

--- RubySol/mark_and_toys.rb
+++ RubySol/mark_and_toys.rb
@@ -1,10 +1,12 @@
 =begin
-Mark and Jane are very happy after having their first kid. Their son is very fond of toys, so Mark wants to buy some. There are  N different toys lying in front of him, tagged with their prices, but he has only $K . He wants to maximize the number of toys he buys with this money.
-
+Mark and Jane are very happy after having their first kid. Their son is very fond of toys, so Mark wants to buy some.
+There are  N different toys lying in front of him, tagged with their prices, but he has only $K . He wants to maximize
+the number of toys he buys with this money.
 Now, you are Mark's best friend and have to help him buy as many toys as possible.
 
 Input Format
-The first line contains two integers, N and K, followed by a line containing N space separated integers indicating the products' prices.
+The first line contains two integers, N and K, followed by a line containing N space separated integers indicating the
+products' prices.
 
 Output Format
 An integer that denotes maximum number of toys Mark can buy for his son.
